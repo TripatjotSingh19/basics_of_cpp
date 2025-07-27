091db985af61853c287e8f680811d1a2acdf8d38
TripatjotSingh19 <tripatjots646@gmail.com>
2025-07-27 15:42:16 +0530

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// This is a simple C++ program that prints "Hello, World!" to the console.
+//Tripatjot Singh, PRN 24070123044
+
+#include<iostream>
+using namespace std;
+int main() {
+      cout << "Hello  World!" << endl;
+    return 0;
+    
+}
+//Output: Hello  World!
